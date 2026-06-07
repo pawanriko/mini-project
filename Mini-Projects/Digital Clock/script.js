f2321d2cd49805d755ab8f91516c5b80c9cf6e5f
Pawan Kumar <zecxoff@gmail.com>
2026-06-07 13:48:54 +0530

    Add files via upload

---
+++ Mini-Projects/Digital Clock/script.js
@@ -0,0 +1,14 @@
+function updateClock() {
+    const now = new Date();
+
+    let hours = String(now.getHours()).padStart(2, "0");
+    let minutes = String(now.getMinutes()).padStart(2, "0");
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("time").textContent =
+        `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
